5e159730700dc11dec0d91f6d4a49c0215a28726
Bruno Simbine <brunosimbine07012002@gmail.com>
2024-12-06 15:53:59 +0200

    sd

--- Program.cs
+++ Program.cs
@@ -33,12 +33,10 @@ builder.WebHost.ConfigureKestrel(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseCors("MyPolicy");
 app.UseHttpsRedirection();
 
